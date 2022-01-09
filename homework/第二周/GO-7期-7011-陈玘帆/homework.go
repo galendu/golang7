73c3547f1dce106aaaf673a836d10e67d5f8abe0
13070131561 <C000819>
2022-01-09 19:01:10 +0800

    Merge branch 'master' of https://gitee.com/magedu/golang7

--- homework/第二周/GO-7期-7011-陈玘帆/homework.go
+++ homework/第二周/GO-7期-7011-陈玘帆/homework.go
@@ -1,1 +1,55 @@
-package GO_7期_7011_陈玘帆
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"strings"
+)
+
+func getSliceDistinctElementsCount(s []int) int {
+	var m map[int]bool
+	m = make(map[int]bool, 100)
+
+	for _, ele := range s {
+		m[ele] = true
+	}
+
+	return len(m)
+}
+
+
+func arr2string(arr []int) string{
+	sb := strings.Builder{}
+
+	for _, ele := range arr{
+		var s string = strconv.Itoa(ele)
+		sb.WriteString(s)
+		sb.WriteString(" ")
+	}
+	mergedSb := sb.String()
+	return mergedSb
+}
+
+
+func main() {
+	fmt.Print("---获取切片中互不相同的元素个数---\n")
+	s := make([]int, 0, 10)
+	for i := 0; i < 100; i++ {
+		randint := rand.Intn(128)
+		s = append(s, randint)
+	}
+
+	sl := getSliceDistinctElementsCount(s)
+	fmt.Printf(" slice distinct elements count: %d\n", sl)
+
+	fmt.Print("---将切片元素转为字符串---\n")
+
+	s1 := make([]int, 0, 10)
+	stringArr := arr2string(s1)
+	fmt.Printf("原切片：%v \n转化为字符串后：%s\n", s1, stringArr)
+
+	stringArr2 := arr2string(s)
+	fmt.Printf("原切片：%v \n转化为字符串后：%s\n", s, stringArr2)
+
+}
