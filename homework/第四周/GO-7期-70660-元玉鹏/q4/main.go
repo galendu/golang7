4789ae2aacdf6025e918d084410380701e2876fc
yuanyp8 <geniusgavin@163.com>
2022-01-25 13:38:42 +0800

    add 元玉鹏's homework 4

---
+++ homework/第四周/GO-7期-70660-元玉鹏/q4/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func square(num interface{}) interface{} {
+	switch v := num.(type) {
+	case float64:
+		return v * v
+	case float32:
+		return v * v
+	case int:
+		return v * v
+	case byte:
+		return v * v
+	default:
+		return errors.New("参数类型异常")
+	}
+}
+
+func main() {
+	fmt.Println(square(12321))
+}
