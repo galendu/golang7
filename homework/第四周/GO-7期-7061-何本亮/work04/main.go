d58222c1375522941f1cad8bb21431f2ffa85a18
wangjianwei <18855038797@163.com>
2022-02-09 14:13:34 +0800

    何本亮第四次作业

---
+++ homework/第四周/GO-7期-7061-何本亮/work04/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func square(num interface{}) interface{} {
+	switch v := num.(type) {
+	case int:
+		return v * v
+	case float32:
+		return v * v
+	case float64:
+		return v * v
+	case byte:
+		return v * v
+	default:
+		return "不支持的类型"
+	}
+}
+
+func main() {
+	num := 2.1
+	s := square(num)
+	fmt.Printf("%v的平方为: %v\n", num, s)
+}
