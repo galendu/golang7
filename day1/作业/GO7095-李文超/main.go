4f17784ae5952ef64007c5a0b6e4cf8722420133
wenchao.li <1746191235@qq.com>
2022-02-10 15:43:18 +0800

    history work commit

--- day1/作业/GO7095-李文超/main.go
+++ day1/作业/GO7095-李文超/main.go
@@ -29,8 +29,8 @@ func BinaryFormat(n int32) string {
 		c >>= 1 //每次右偏移量1和n的二进制进行比对
 		//fmt.Printf("当前二进制是%b<<<<<<<<<<\n",c)
 	}
-	fmt.Printf("%d的二进制是%b,%d的十进制是%d\n", n, a, n, n)
-	fmt.Printf("2的31次方的二进制是%b,十进制是%d\n", c, c)
+	//fmt.Printf("%d的二进制是%b,%d的十进制是%d\n", n, a, n, n)
+	//fmt.Printf("2的31次方的二进制是%b,十进制是%d\n", c, c)
 
 	return "换算成32进制是" + sb.String()
 }
