c543e985d2dbc3156699e472167fb24b94050a5d
王豪杰 <>
2022-02-11 18:19:44 +0800

    day4 作业

---
+++ homework/第四周/GO-7期-7087-王豪杰/workhome1/main.go
@@ -0,0 +1,18 @@
+package workhome1
+
+import "fmt"
+
+func Square(num interface{}) interface{} {
+	switch result := num.(type) {
+	case int:
+		return result * result
+	case byte:
+		return result * result
+	case float32:
+		return fmt.Sprintf("%.2f", result*result)
+	case float64:
+		return fmt.Sprintf("%.2f", result*result)
+	default:
+		return "类型不正确..."
+	}
+}
